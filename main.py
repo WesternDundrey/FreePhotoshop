630f42616a22c2e01719e102ec4a77c7366708a2
westerndundrey <hijackedpuffin@gmail.com>
2024-07-12 21:40:19 +0800

    MinSize

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class App(ctk.CTk):
         super().__init__()
         ctk.set_appearance_mode('dark')
         self.geometry('1000x600')
+        self.title('PhotoEdit')
+
+        self.minsize(800,500)
         # run
         self.mainloop()
 
